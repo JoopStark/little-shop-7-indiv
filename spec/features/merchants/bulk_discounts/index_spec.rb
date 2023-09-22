01b6cc863d93c94de72a8d4129818f27c09e38d3
Joop Stark <stark.joop@gmail.com>
2023-09-22 13:20:55 -0600

    test feat: delete instance of bulk discount

--- spec/features/merchants/bulk_discounts/index_spec.rb
+++ spec/features/merchants/bulk_discounts/index_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe "Merchant Item Index page" do
     @merchant_1 = Merchant.create!(name: "Kiki")
     @merchant_2 = Merchant.create!(name: "Dave")
 
-    BulkDiscount.create!(name: "Dozen", discount: 0.10, threshold: 12, merchant: @merchant_1)
-    BulkDiscount.create!(name: "Baker", discount: 0.20, threshold: 13, merchant: @merchant_1)
+    @dozen = BulkDiscount.create!(name: "Dozen", discount: 0.10, threshold: 12, merchant: @merchant_1)
+    @baker = BulkDiscount.create!(name: "Baker", discount: 0.20, threshold: 13, merchant: @merchant_1)
     BulkDiscount.create!(name: "Gross", discount: 0.30, threshold: 144, merchant: @merchant_1)
     BulkDiscount.create!(name: "Nope", discount: 0.99, threshold: 999, merchant: @merchant_2)
   end
@@ -32,5 +32,19 @@ RSpec.describe "Merchant Item Index page" do
     click_link 'Create New Discount'
 
     expect(page).to have_current_path(new_merchant_bulk_discount_path(@merchant_1))
+  end
+
+  it "has a working button to delete a discount" do
+    visit merchant_bulk_discounts_path(@merchant_1)
+
+    expect(page).to have_content("Baker")
+    expect(page).to have_content("20.0%")
+    expect(page).to have_button("Delete #{@baker.name}")
+
+    click_button "Delete #{@baker.name}"
+
+    expect(page).to_not have_content("Baker")
+    expect(page).to_not have_content("20.0%")
+    expect(page).to_not have_button("Delete #{@baker.name}")
   end
 end
