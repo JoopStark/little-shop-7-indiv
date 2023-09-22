eb2f3ec97c778a499fbaa0ffc3d20bce01cdf220
Joop Stark <stark.joop@gmail.com>
2023-09-22 10:58:17 -0600

    feat: Bulk discount create and sad paths

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -4,16 +4,30 @@ class BulkDiscountsController < ApplicationController
   end
 
   def show
-    @discount = Discount.find(params[:id])
+    @bulk_discount = BulkDiscount.find(params[:id])
   end
 
   def new
+    @merchant = Merchant.find(params[:merchant_id])
   end
 
   def create
+    bulk_discount = BulkDiscount.create(bulk_discount_params)
+    if bulk_discount.save
+      redirect_to "/merchants/#{params[:merchant_id]}/bulk_discounts"
+    else
+      redirect_to "/merchants/#{params[:merchant_id]}/bulk_discounts/new"
+      flash[:alert] = "Error: #{error_message(bulk_discount.errors)}"
+    end
   end
 
   def delete
     Discount.destroy(params[:id])
+  end
+
+  private
+
+  def bulk_discount_params
+    params.permit(:name, :discount, :threshold, :merchant_id)
   end
 end
